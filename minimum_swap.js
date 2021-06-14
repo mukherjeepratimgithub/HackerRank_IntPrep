34f59672cf28fd278fe5f09ae1cbfd0c898bc797
Pratim Mukherjee <pratimmukherjeeiem@gmail.com>
2021-06-15 01:24:52 +0530

    minimum swap

---
+++ minimum_swap.js
@@ -0,0 +1,43 @@
+function minSwaps(arr,N)
+    {
+        let ans = 0;
+        let temp = [...arr];
+        temp.sort(function(a,b){return a-b;});
+        for (let i = 0; i < N; i++)
+        {
+  
+            // This is checking whether
+            // the current element is
+            // at the right place or not
+            if (arr[i] != temp[i])
+            {
+                ans++;
+  
+                // Swap the current element
+                // with the right index
+                // so that arr[0] to arr[i] is sorted
+                swap(arr, i, indexOf(arr, temp[i]));
+            }
+        }
+        return ans;
+    }
+     
+    function swap(arr,i,j)
+    {
+         let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;   
+    }
+     
+    function indexOf(arr,ele)
+    {
+        for (let i = 0; i < arr.length; i++)
+        {
+            if (arr[i] == ele) {
+                return i;
+            }
+        }
+        return -1;
+    }
+     
+  
